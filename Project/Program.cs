bc564954a43992a50e60127384e17fdafa037cae
Aigul Aiupova <aigul.sadreeva@gmail.com>
2023-02-26 08:14:00 +0300

    Метод ввода строки, Перевод строки в массив строк, Вывод массива строк в консоль

--- Project/Program.cs
+++ Project/Program.cs
@@ -7,3 +7,28 @@
 // ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
 // ['1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] ->
+
+string InsertString(string text) //метод ввода строки пользователем
+{
+    Console.WriteLine(text);
+    string result = Console.ReadLine();
+    return result;
+}
+
+string[] StringToArrayOfStrings(string userArray) //перевод строки в массив строк
+{
+    string str = userArray;
+    string[] arrOfStrings = str.Split(",");
+    return arrOfStrings;
+}
+
+void PrintArrOfStrings(string[] array) // вывод массива строк в консоль
+{
+    Console.Write("["); 
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write(array[i] + ", "); 
+        else Console.Write(array[i]); 
+    }
+    Console.Write("]");
+}
